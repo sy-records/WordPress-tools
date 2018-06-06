0756d4c53d2e2e3be6d22513b84c938db137e06b
shenyan <52o@qq52o.cn>
2018-06-06 09:46:29 +0800

    添加友链

---
+++ functions-diy.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @authors ShenYan (52o@qq52o.cn)
+ * @date    2018-06-06 09:43:21
+ * @boke    https://qq52o.me
+ */
+//添加友链按钮
+add_filter('pre_option_link_manager_enabled', '__return_true');
