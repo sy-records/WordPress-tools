cdb81fd0a9f9cb8282a1c33406c88b521cd29331
shenyan <52o@qq52o.cn>
2018-08-26 09:38:49 +0800

    添加随便看看

--- functions-diy.php
+++ functions-diy.php
@@ -326,3 +326,25 @@ function link_jump($content){
 	}
 	return $content;
 }
+
+/**
+ * WordPress添加随便看看 | 沈唁志
+ * link：https://qq52o.me/165.html
+ */
+function random_postlite() {
+    global $wpdb;
+    $query = "SELECT ID FROM $wpdb->posts WHERE post_type = 'post' AND post_password = '' AND post_status = 'publish' ORDER BY RAND() LIMIT 1";
+    if ( isset( $_GET['random_cat_id'] ) ) {
+        $random_cat_id = (int) $_GET['random_cat_id'];
+        $query = "SELECT DISTINCT ID FROM $wpdb->posts AS p INNER JOIN $wpdb->term_relationships AS tr ON (p.ID = tr.object_id AND tr.term_taxonomy_id = $random_cat_id) INNER JOIN $wpdb->term_taxonomy AS tt ON(tr.term_taxonomy_id = tt.term_taxonomy_id AND taxonomy = 'category') WHERE post_type = 'post' AND post_password = '' AND post_status = 'publish' ORDER BY RAND() LIMIT 1";
+    }
+    if ( isset( $_GET['random_post_type'] ) ) {
+        $post_type = preg_replace( '|[^a-z]|i', '', $_GET['random_post_type'] );
+        $query = "SELECT ID FROM $wpdb->posts WHERE post_type = '$post_type' AND post_password = '' AND post_status = 'publish' ORDER BY RAND() LIMIT 1";
+    }
+    $random_id = $wpdb->get_var( $query );
+    wp_redirect( get_permalink( $random_id ) );
+    exit;
+}
+if ( isset( $_GET['random'] ) )
+add_action( 'template_redirect', 'random_postlite' );
