64dfec3896480aaea76e85e630c049f2196ed8f5
沈唁 <52o@qq52o.cn>
2018-11-17 16:32:08 +0800

    Update functions-diy.php

--- functions-diy.php
+++ functions-diy.php
@@ -675,3 +675,17 @@ function git_upload_filter($file) {
         return $file;
 }
 add_filter('wp_handle_upload_prefilter', 'git_upload_filter');
+
+// 给评论框加上图片按钮，可评论回复图片
+function auto_comment_image( $comment ) {
+      global $allowedtags;
+      $size = auto;
+      $content = $comment["comment_content"];
+      // alt部分自行填写
+      $content = preg_replace('/((https|http|ftp):\/\/){1}.+?.(jpg|gif|bmp|bnp|png)$/is','<img src="$0" alt=""  style="width:'.$size.'; height:'.$size.'" />',$content);
+      //允许发布img标签
+      $allowedtags['img'] = array('src' => array (), 'alt' => array (),'class' =>array());
+      // 重新给$comment赋值
+      $comment["comment_content"] = $content;
+      return $comment;
+}
